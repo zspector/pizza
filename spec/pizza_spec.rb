b2ed92a93ed5fd3d9c9a668f8eca8b681de1a665
Zachary Spector <zjspector@gmail.com>
2014-04-23 14:50:04 -0500

    Add failing test to Pizza.initialize

--- spec/pizza_spec.rb
+++ spec/pizza_spec.rb
@@ -4,6 +4,18 @@ describe Pizza do
   it "exists" do
     expect(Pizza).to be_a(Class)
   end
+
+  describe '.initialize' do
+    it 'records all of the toppings' do
+      toppings = [
+        Topping.new('mushrooms', vegetarian: true),
+        Topping.new('pepperoni')
+      ]
+      pizza = Pizza.new(toppings)
+
+      expect(pizza.toppings).to eq(toppings)
+    end
+  end
 end
 
 describe Topping do
